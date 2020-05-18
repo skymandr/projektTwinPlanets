1ed3fa9ee4d8625948f6c97f3b2a52f5f2712831
Andreas Skyman <skymandr@fripost.org>
2020-05-18 12:46:31 +0200

    transformations interface

---
+++ transformations.py
@@ -0,0 +1,26 @@
+def pixel_to_lat_lon(x, y, width, height):
+    raise NotImplementedError
+
+
+def lat_lon_to_cassini(lat, lon):
+    raise NotImplementedError
+
+
+def lat_lon_to_transverse_mercator(lat, lon):
+    raise NotImplementedError
+
+
+def lat_lon_to_sinusoidal(lat, lon):
+    raise NotImplementedError
+
+
+def peel_to_daisy_gore(x, y):
+    raise NotImplementedError
+
+
+def peel_to_gore(x, y):
+    raise NotImplementedError
+
+
+def gore_to_pixel(x, y):
+    raise NotImplementedError
